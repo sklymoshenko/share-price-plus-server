2ba7dbbbfb51f7ede91000f3e021bdf54ded85cd
Stanislav Klymoshenko <s.klymoshenko@gmail.com>
2022-02-10 19:21:46 +0100

    rewrite event schema with participant schema and virtuals and loaners schema

--- src/models/event.ts
+++ src/models/event.ts
@@ -1,6 +1,40 @@
 import * as Mongoose from "mongoose";
 import { ISpEvent } from "src/types/entities/event";
 import { ISpParticipant } from "src/types/entities/user";
+import { ISpParticipantVirtualThis } from "src/types/shared";
+
+const LoanerSchema: Mongoose.Schema = new Mongoose.Schema({
+  name: {
+    type: String
+  },
+  paid: {
+    type: Number,
+    default: 0
+  }
+});
+
+const ParticipantSchema: Mongoose.Schema = new Mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  paid: {
+    type: Number,
+    default: 0
+  },
+
+  loaners: [LoanerSchema]
+});
+
+ParticipantSchema.virtual("ows").get(function (this: ISpParticipantVirtualThis): number {
+  const { each } = this.parent();
+  return this.paid > each ? 0 : each - this.paid;
+});
+
+ParticipantSchema.virtual("exceed").get(function (this: ISpParticipantVirtualThis): number {
+  const { each } = this.parent();
+  return this.paid > each ? this.paid - each : 0;
+});
 
 const EventSchema: Mongoose.Schema = new Mongoose.Schema(
   {
@@ -8,10 +42,7 @@ const EventSchema: Mongoose.Schema = new Mongoose.Schema(
       type: String,
       required: true
     },
-    participants: {
-      type: Array,
-      default: []
-    },
+    participants: [ParticipantSchema],
     isClosed: {
       type: Boolean,
       default: false
--- src/types/shared.ts
+++ src/types/shared.ts
@@ -1,4 +1,6 @@
 import { Request } from "express";
+import { ISpEvent } from "./entities/event";
+import { ISpParticipant } from "./entities/user";
 
 export interface IError {
   message: string;
@@ -6,4 +8,8 @@ export interface IError {
 
 export interface IContext {
   req: Request;
+}
+
+export interface ISpParticipantVirtualThis extends ISpParticipant {
+  parent(): ISpEvent;
 }
