ec756140a503d457a6f7d90177ecfdc2ab090ee8
Stanislav Klymoshenko <s.klymoshenko@gmail.com>
2022-02-10 21:35:48 +0100

    set explicit path

--- src/server.ts
+++ src/server.ts
@@ -75,6 +75,7 @@ async function startServer() {
         secret: SESSION_SECRET,
         saveUninitialized: false,
         cookie: {
+          path: "/",
           maxAge: 1000 * 60 * 60 * 24,
           secure: process.env.NODE_ENV === "production",
           httpOnly: false,
